d15571741528d03a64644f6e2aff301d242bbeab
John Rinehart <john@tinkermode.com>
2019-09-05 09:43:41 -0700

    renamed tile_list -> tiles

--- puzzle.py
+++ puzzle.py
@@ -2,20 +2,19 @@
 
 # Puzzle class defines the structure and behavior of a collection of Tile objects. The state of a Puzzle is not mutated directly. Rather, the state of the Tile objects attached to a Puzzle instance are each mutable, implicitly modifiying the state of the Puzzle object. Thus, all methods of the Puzzle class are read-only.
 class Puzzle(object):
-    """
-    verifies the overall picture
-    """
-    def __init__(self, tile_list):
-        self.tile_list = tile_list
+    # constructor
+    # tiles is a list of tiles
+    def __init__(self, tiles):
+        self.tiles = tiles
 
     def verify(self):
         """the verify implementation of the overall
         puzzle"""
-        assert isinstance(self.tile_list, list)
-        assert len(self.tile_list) == 9
+        assert isinstance(self.tiles, list)
+        assert len(self.tiles) == 9
 
         flag = True
-        for i in self.tile_list:
+        for i in self.tiles:
             if i.get_top() != 'a':
                 flag = False
             if i.get_right() != 'b':
@@ -28,7 +27,7 @@ class Puzzle(object):
 
     def __str__(self):
         return f"""
-            {str(self.tile_list[0])} {str(self.tile_list[1])} {str(self.tile_list[2])}
-            {str(self.tile_list[3])} {str(self.tile_list[4])} {str(self.tile_list[5])}
-            {str(self.tile_list[6])} {str(self.tile_list[7])} {str(self.tile_list[8])}
+            {str(self.tiles[0])} {str(self.tiles[1])} {str(self.tiles[2])}
+            {str(self.tiles[3])} {str(self.tiles[4])} {str(self.tiles[5])}
+            {str(self.tiles[6])} {str(self.tiles[7])} {str(self.tiles[8])}
         """
