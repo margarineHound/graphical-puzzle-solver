c7198fcd98d5b222aa264d815c3d849f79339388
margarineHound <margarineHound@users.noreply.github.com>
2019-09-06 15:49:12 -0400

    modified the tile input method,

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
-import tile
-from puzzle import puzzle
+from tile import Tile
+import puzzle
 from square import *
+import csv
+
 def main():
 
-    # TODO: Definine each tile as a line in a CSV file, where each column is associated with one edge (0th column: top edge, 1st column: right edge, etc.)
+    try:
+        csv_file = open('csvfile.csv', newline='')
+    except OSError as e:
+        print(f"File {e.filename} could not be opened")
 
-    # TODO: Define a function in square.py which accepts a file handle and returns a list of tiles.
-    delt = [['a', 'b', 'c', 'd'], ['b','c', 'd', 'a'], ['c', 'd', 'a', 'b'], ['d', 'a', 'b', 'c'], ['a', 'b', 'c', 'd'], ['b','c', 'd', 'a'], ['c', 'd', 'a', 'b'], ['d', 'a', 'b', 'c'], ['d', 'a', 'b', 'c']]
-    effect = tile_const(delt) # delt and effect are confusing names. Haha, pick not-confusing ones.
+    tile_list = csvparser(csv_file)
+    tiles = tile_const(tile_list)
 
-    check(effect) # See TODO above this function declaration. I'm thinking we can omit this.
+    grid_const = puzzle.Puzzle(tiles)
+    check(grid_const) # See TODO above this function declaration. I'm thinking we can omit this.
 
-    ver = puzzle(effect) # ver sounds like "verify" when this is actually a Puzzle instance. It would be better to call this "puz" or similar.
+    # ver = Puzzle(grid_const) # ver sounds like "verify" when this is actually a Puzzle instance. It would be better to call this "puz" or similar.
 
     # The below is kind of what we want
-    if ver.verify():
+    if grid_const.verify():
         print("Hell yes!")
     else:
         print("Fuck me... Trying another orientation of tiles...")
