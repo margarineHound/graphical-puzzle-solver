197d23314c3a4b884872d33c38de3d63fb2aab1f
John Rinehart <john@tinkermode.com>
2019-09-07 10:30:41 -0700

    changed file name on disk and also its reference variable name in main

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
 from tile import Tile
-import puzzle
+from puzzle import *
 from square import *
 import csv
 
 def main():
 
     try:
-        csv_file = open('csvfile.csv', newline='')
+        fh = open('tiles.csv', newline='') # file handle
     except OSError as e:
         print(f"File {e.filename} could not be opened")
 
-    tile_list = csvparser(csv_file)
+    tile_list = csvparser(fh)
     tiles = tile_const(tile_list)
 
-    grid_const = puzzle.Puzzle(tiles)
-    check(grid_const) # See TODO above this function declaration. I'm thinking we can omit this.
-
-    # ver = Puzzle(grid_const) # ver sounds like "verify" when this is actually a Puzzle instance. It would be better to call this "puz" or similar.
+    puz = Puzzle(tiles)
+    check(puz)
 
     # The below is kind of what we want
-    if grid_const.verify():
+    if puz.verify():
         print("Hell yes!")
     else:
         print("Fuck me... Trying another orientation of tiles...")
